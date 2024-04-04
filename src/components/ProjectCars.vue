6d879f5391bb552eec5e7498ee5d4b476bfa9b5f
ChristianBaciu <christian3110@outlook.it>
2024-04-05 01:00:43 +0200

    arrayProject stampato in pagina

---
+++ src/components/ProjectCars.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from 'axios';
+
+    export default{
+        name: 'ProjectCars',
+        data(){
+            return{
+                arrayProject:[ ]
+            }
+        },
+        methods:{
+            getProjects(){
+                axios.get('http://127.0.0.1:8000/api/apiProjects').then(res =>{
+                    console.log(res.data.projets);
+
+                    this.arrayProject = res.data.projets;
+                })
+            }
+        },
+        mounted(){
+            this.getProjects();
+        }
+    }
+</script>
+
+<!-- ------------------------------------------------------------------- -->
+
+<template>
+    <h1>PROJECTS</h1>
+    
+    <p v-for="(element, index) in arrayProject" :key="element.id">
+        {{ element.titolo }}
+    </p>
+</template>
+
+<!-- ------------------------------------------------------------------- -->
+
+<style scoped>
+
+</style>
