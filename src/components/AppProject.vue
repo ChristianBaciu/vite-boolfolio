a49f14a1d94a401a0b7e613321e5e2ed2803ebd1
ChristianBaciu <christian3110@outlook.it>
2024-04-09 19:22:19 +0200

    aggiunte informazioni della singola card nel file ProjectSingle

--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -23,9 +23,8 @@
 <template>
 
     <router-link :to="{name: 'single-project', params: {titolo: titolo}}">
-
         <div class="card my-4" style="width: 18rem;">
-            <img :src='`http://127.0.0.1:8000/storage/${cover_image}`' class="card-img-top" alt="...">
+            <img v-if="cover_image" :src='`http://127.0.0.1:8000/storage/${cover_image}`' class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{titolo}}</h5>
                 <p class="card-text">{{contenuto}}</p>
@@ -37,20 +36,7 @@
 
             </div>
         </div>
-
     </router-link>
-
-    
-
-    <!-- 
-    <p v-for="(element, index) in arrayProject" :key="element.id">
-        <router-link :to="{name: 'single-project', params: {titolo: element.titolo}}">
-            <h3>{{ element.titolo }}</h3>
-            <p>{{ element.contenuto }}</p>
-        </router-link>
-    </p>
-     -->
-
 
 </template>
 
--- src/pages/ProjectSingle.vue
+++ src/pages/ProjectSingle.vue
@@ -1,16 +1,29 @@
 <script>
+import axios from 'axios';
 
     export default{
+        name: 'ProjectSingle',
         components:{
             
         },
         data(){
             return{
+                project: {}
             }
         },
         methods:{
+            getProjectSingle(){
+                axios.get(`http://127.0.0.1:8000/api/apiProjects/${this.$route.params.titolo}`).then(res =>{
+                    if(res.data.success){
+                        this.project = res.data.project
+                    }else{
+                    }
+                    // console.log(res)
+                })
+            }
         },
         mounted(){
+            this.getProjectSingle()
         }
     }
 </script>
@@ -19,6 +32,23 @@
 
 <template>
     <h1 class="text-center mt-3">PROJECTSINGLE</h1>
+
+    <div class="card my-4" style="width: 18rem;">
+        <img v-if="project && project.cover_image" :src="`http://127.0.0.1:8000/storage/${project.cover_image}`" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{project.titolo}}</h5>
+            <p class="card-text">{{project.contenuto}}</p>
+
+            <!-- se project è definito e ha un type stamperà nella pagina -->
+            <p class=" m-1" v-if="project && project.type">{{project.type.nome}}</p>
+
+            <!-- se in technologies ci sono elementi allora stamperà nella pagina le chiavi-->
+            <span class=" m-1" v-if="project?.technologies?.length" v-for="(technology, index) in project.technologies" :key="technology.id">
+                {{technology.name}}
+            </span>
+        </div>
+    </div>
+
 </template>
 
 <!-- ------------------------------------------------------------------- -->
